01b90ddb8f81cd18c71e5dfb5ffa4e0c5baa8df2
nn-advith <advith.nn@gmail.com>
2025-10-21 13:04:14 +0530

    doc generator script for golang

---
+++ go/docgen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASEDIR=$(cd $(dirname $0) && pwd)
+
+set -e
+
+opfile=${1:-"${BASEDIR}/doc.txt"}
+packages=$(go list ./...)
+
+> "${opfile}"
+
+for i in $packages; do
+	echo "=========== $i ===========" >> "${opfile}"
+	go doc -all -u "$i" >> "${opfile}"
+	echo "" >> "${opfile}"
+done
+
+echo "Doc generated"
